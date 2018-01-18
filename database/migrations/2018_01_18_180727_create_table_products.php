fbe5a8ccc46b8caf8788192f7dc69abc2b4a13ea
Rodolfo Moreira <rodolformoreira@gmail.com>
2018-01-18 16:14:37 -0200

    Criação da tabela products

---
+++ database/migrations/2018_01_18_180727_create_table_products.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->longText('description');
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
